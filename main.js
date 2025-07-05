b47599bebe2cb4ff3e83d14eaac34e387d88a2d6
Justdan <muhammadani606@gmail.com>
2025-07-05 15:52:28 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons' // Import all solid icons
+import './style.css'
+
+library.add(fas) // Ensure all solid icons are available
+
+const app = createApp(App)
+
+app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
